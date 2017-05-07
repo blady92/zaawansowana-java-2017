801d1fb18b337ac4d91f917f961ebd0e4bfba028
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2017-05-07 12:36:39 +0200

    Add second constructor for Field implementation
    In future there will be some purposes, where state of field in not
    important

--- src/main/java/ships/model/FieldImpl.java
+++ src/main/java/ships/model/FieldImpl.java
@@ -17,6 +17,13 @@ public class FieldImpl implements Field {
         this.col = col;
     }
 
+    public FieldImpl(int row, int col) {
+        this.state = State.EMPTY;
+        this.row = row;
+        this.col = col;
+        this.attacked = Boolean.FALSE;
+    }
+
     public int getRow() {
         return row;
     }
@@ -25,14 +32,27 @@ public class FieldImpl implements Field {
         return col;
     }
 
+    /**
+     * Informs if field is occuped by a ship
+     *
+     * @return
+     */
     public Boolean isShipHere() {
         return state == State.SHIP;
     }
 
+    /**
+     * Informs if field had been already shot by opponent
+     *
+     * @return <b>true</b> if field had been attacked
+     */
     public Boolean isAttacked() {
         return attacked;
     }
 
+    /**
+     * @param state the state to set
+     */
     public void setState(State state) {
         this.state = state;
     }
