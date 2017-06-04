8c80d0b838fc01f01b3b8a2e828cf2891dacc797
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2017-06-04 11:22:11 +0200

    Use Connection interface for multiplayer

--- src/main/java/ships/controller/PlayerHostedGame.java
+++ src/main/java/ships/controller/PlayerHostedGame.java
@@ -5,13 +5,34 @@
  */
 package ships.controller;
 
+import ships.exception.ShipGameException;
 import ships.model.Ship;
+import ships.view.OpponentMapView;
+import ships.view.PlayerMapView;
 
 /**
  *
  * @author r4pt0r
  */
 public class PlayerHostedGame extends Game {
+
+    private Connection conn;
+
+    public PlayerHostedGame() {
+        super();
+
+        throw new UnsupportedOperationException("Do we need it?");
+    }
+
+    public PlayerHostedGame(
+            PlayerMapView playerMapView, OpponentMapView opponentMapView,
+            Integer port
+    ) throws ShipGameException {
+        super(playerMapView, opponentMapView);
+
+        conn = new TCPServerConnection(port);
+        throw new UnsupportedOperationException("What about ship placement");
+    }
 
     @Override
     public void startPlacement(Ship.Size size) {
@@ -25,7 +46,7 @@ public class PlayerHostedGame extends Game {
 
     @Override
     protected Boolean playerShooting() {
-        while(playerMoveQueue.isEmpty()) {
+        while (playerMoveQueue.isEmpty()) {
             //wait until player performs a move
         }
         throw new UnsupportedOperationException("TODO: send the move to client");
@@ -36,7 +57,5 @@ public class PlayerHostedGame extends Game {
     protected Boolean opponentShooting() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    
-    
+
 }
